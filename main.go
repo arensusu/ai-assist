ffb0f8f7a6f24403e1343214a3e93e1404753aef
arensusu <willy9966@gmail.com>
2024-09-14 10:16:21 +0000

    update prompt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -31,6 +32,9 @@ func main() {
 
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
+				var uid string
+				uid = event.Source.UserID
+
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
 					response, err := callGemini(ctx, message.Text)
@@ -39,6 +43,7 @@ func main() {
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("出現問題，請稍後再試")).Do()
 						return
 					}
+					recordMessage(uid, []byte(message.Text))
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(response)).Do()
 				}
 			}
@@ -66,11 +71,10 @@ func callGemini(ctx context.Context, message string) (string, error) {
 	// Get the model
 	model := client.GenerativeModel("gemini-1.5-flash")
 
-	// Create a prompt
-	prompt := message
+	systemPrompt := "You are a helpful assistant. Please answer the user's question with user's language, do not use any other language. Not use any format like markdown."
 
 	// Generate content
-	resp, err := model.GenerateContent(ctx, genai.Text(prompt))
+	resp, err := model.GenerateContent(ctx, genai.Text(systemPrompt), genai.Text(message))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -84,4 +88,15 @@ func callGemini(ctx context.Context, message string) (string, error) {
 	}
 
 	return string(content), nil
+}
+
+func recordMessage(uid string, message []byte) error {
+	file, err := os.Create(fmt.Sprintf("/tmp/ai_%s.txt", uid))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(message)
+	return err
 }
